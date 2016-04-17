ff9e96714a1bcd0441124e830c3bc0a2ae1937d5
kataraptor <kataraptor@gmail.com>
2016-04-17 02:49:49 -0600

    fixed the punched in icon

--- app/src/main/java/club/yft/whoshome/HomeActivity.java
+++ app/src/main/java/club/yft/whoshome/HomeActivity.java
@@ -1,5 +1,6 @@
 package club.yft.whoshome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,7 +23,7 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "You are punched in", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -44,6 +45,8 @@ public class HomeActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent gameMode = new Intent(this, SettingsActivity.class);
+            startActivity(gameMode);
             return true;
         }
         return super.onOptionsItemSelected(item);
